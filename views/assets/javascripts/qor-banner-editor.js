f3a5ccb089b89377561f0a0298c40d6a9b5f0f4e
binku <binku87@gmail.com>
2016-04-15 13:42:30 +0800

    banner_editor: add tmpl and code to make vee work

---
+++ views/assets/javascripts/qor-banner-editor.js
@@ -0,0 +1,41 @@
+  $(document).ready(function(){
+    $(".bannerEditor").vee({
+      width: "960px",
+      height: "400px",
+      datas : {
+        "sub_heading" : {
+          "title" : "Sub Header",
+          "template" : '<em style="color" {{color}};">{{text}}</em>',
+          "keys" : {
+            "text" : { "default" : "SUB-HEADING GOES HERE", "type" : "input" },
+            "color" : { "default" : "#7dd4c9", "type" : "color" }
+          }
+        },
+        "heading" : {
+          "title" : "Header",
+          "template" : '<h1 style="color" {{color}};">{{text}}</h1>',
+          "keys" : {
+            "text" : { "default" : "HEADER GOES HERE", "type" : "input" },
+            "color" : { "default" : "#FFFFFF", "type" : "color" }
+          }
+        },
+        "bold_heading" : {
+          "title" : "Bold Header",
+          "template" : '<h1 style="color" {{color}};"><strong>{{text}}</strong></h1>',
+          "keys" : {
+            "text" : { "default" : "BOLD HEADER GOES HERE", "type" : "input" },
+            "color" : { "default" : "#000000", "type" : "color" }
+          }
+        },
+        "button" : {
+          "title" : "Button",
+          "template" : '<a href="{{link}}" class="btn" title="{{text}}" target="{{target}}">{{text}}</a>',
+          "keys" : {
+            "text" : { "default" : "BUTTON TEXT GOES HERE", "type" : "input" },
+            "link" : { "default" : "http://www.asicsamerica.com/", "type" : "input" },
+            "target" : {"default" : "", "type" : "select", "options" : ["", "_blank"]}
+          }
+        }
+      }
+    });
+  });
